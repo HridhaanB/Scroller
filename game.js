800d1e27ff3d7ef8006ebda7ae119e049569326b
HridhaanB <hridhaan.s.banerjee@gmail.com>
2025-08-18 18:59:13 -0400

    Update game.js
    add some code?

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
 alert("Hi!")
+var tilemap = {
+  rows: 500,
+  cols: 500,
+  background: new Map(),
+  middleground: new Map(),
+  foreground: new Map(),
+  getTile: function (layer, row, column) {
+    if (layer===-1) {
+      return background.get([row, column]);
+    } else if (layer===0) {
+      return middleground.get([row, column]);
+    } else if (layer===1) {
+      return foreground.get([row, column]);
+    }
+  }
+}
