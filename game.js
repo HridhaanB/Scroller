e48d997d7731e05bf7cd4a72a4db45c5b3df2434
HridhaanB <hridhaan.s.banerjee@gmail.com>
2025-08-19 18:52:30 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 alert("Hi!")
+
 var tilemap = {
   rows: 500,
   cols: 500,
@@ -7,11 +8,27 @@ var tilemap = {
   foreground: new Map(),
   getTile: function (layer, row, column) {
     if (layer===-1) {
-      return background.get([row, column]);
+      return this.background.get([row, column]);
     } else if (layer===0) {
-      return middleground.get([row, column]);
+      return this.middleground.get([row, column]);
     } else if (layer===1) {
-      return foreground.get([row, column]);
+      return this.foreground.get([row, column]);
     }
   }
+}
+var position = {
+  x: 250,
+  y:250
+}
+var player = {
+  //pos: 
+}
+const canvasSize = c;
+const stepSize = c/10
+
+function update(pos, c, s) {
+  if (Keyboard.isdown(Keyboard.Left)) {pos.x -= s;}
+  if (Keyboard.isdown(Keyboard.Right)) {pos.x += s;}
+  if (Keyboard.isdown(Keyboard.Up)) {pos.y += s;}
+  if (Keyboard.isdown(Keyboard.Down)) {pos.y -= s;}
 }
