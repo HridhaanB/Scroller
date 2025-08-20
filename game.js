a220b68a7d679aa1f82c72019af7e8e74f1aad68
HridhaanB <hridhaan.s.banerjee@gmail.com>
2025-08-20 18:05:46 -0400

    Update game.js
    :( It's harder than I thought

--- game.js
+++ game.js
@@ -1,4 +1,24 @@
 alert("Hi!")
+var Keyboard = {
+  L: 37,
+  R: 39,
+  U: 38,
+  D: 40,
+  keys: {},
+  listen: function (keys) {
+    pass;
+  },
+  keyDown: function(key) {
+    pass;
+  },
+  keyUp: function(key) {
+    pass;
+  }
+}
+
+
+
+
 
 var tilemap = {
   rows: 500,
@@ -20,15 +40,31 @@ var position = {
   x: 250,
   y:250
 }
-var player = {
-  //pos: 
-}
+var Player = {
+  pos: {
+    x: 250,
+    y: 250
+  },
+  speed: 1
+};
 const canvasSize = c;
-const stepSize = c/10
+const stepSize = c/10;
 
-function update(pos, c, s) {
-  if (Keyboard.isdown(Keyboard.Left)) {pos.x -= s;}
-  if (Keyboard.isdown(Keyboard.Right)) {pos.x += s;}
-  if (Keyboard.isdown(Keyboard.Up)) {pos.y += s;}
-  if (Keyboard.isdown(Keyboard.Down)) {pos.y -= s;}
+function update(player, c, s) {
+  if (Keyboard.isdown(Keyboard.L)) {player.pos.x -= s;}
+  if (Keyboard.isdown(Keyboard.R)) {player.pos.x += s;}
+  if (Keyboard.isdown(Keyboard.U)) {player.pos.y += s;}
+  if (Keyboard.isdown(Keyboard.D)) {player.pos.y -= s;}
+}
+
+function renderLayer(player, map, layer) {
+  var startCol = ;
+  var endCol = ;
+  var startRow = ;
+  var endRow = ;
+  for (var c=startCol; c<=endCol; c++) {
+    for (var r=startRow; r<=endRow; r++) {
+      f
+    }
+  }
 }
